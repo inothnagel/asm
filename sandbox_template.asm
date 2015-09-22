89ae5a7fd3c647731d4135d5ace8c0825058d590
Ignatius Nothnagel <inothnagel@gmail.com>
2015-09-22 21:17:40 +0200

    Add sandbox template and makefile.

---
+++ sandbox_template.asm
@@ -0,0 +1,16 @@
+; Sandbox asm file template to start experimenting from
+
+SECTION .data
+SECTION .text
+
+  global _start
+
+_start:
+  nop
+
+  mov eax,1		; Code for Exit Syscall
+  mov ebx,0		; Return a code of zero	
+  int 80H		; Make kernel call
+
+SECTION .bss
+
