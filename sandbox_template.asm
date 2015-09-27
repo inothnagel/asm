3a6dfd0e5dfe9fc531f5802b1aa68215f92e352e
Ignatius Nothnagel <inothnagel@gmail.com>
2015-09-27 12:45:01 +0200

    Add tabs to sandbox template.

--- sandbox_template.asm
+++ sandbox_template.asm
@@ -3,14 +3,14 @@
 SECTION .data
 SECTION .text
 
-  global _start
+	global _start
 
 _start:
-  nop
-
-  mov eax,1		; Code for Exit Syscall
-  mov ebx,0		; Return a code of zero	
-  int 80H		; Make kernel call
+	nop
+	
+	mov eax,1		; Code for Exit Syscall
+	mov ebx,0		; Return a code of zero	
+	int 80H		; Make kernel call
 
 SECTION .bss
 
